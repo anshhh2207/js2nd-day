bc84202af9d8b570aa0ec76303fb97b6822ab0ee
anshhh2207 <150226018+anshhh2207@users.noreply.github.com>
2024-05-22 14:26:22 +0530

    Add files via upload

---
+++ anshika5/script.js
@@ -0,0 +1,61 @@
+// if(5<10){
+//     console.log("5 is less");
+
+// }
+// else{
+//     console.log("10 is greater");
+// }
+// let age=10;
+// if (age>18) {
+//     console.log("You are eligible for Driving license");
+//  } else {
+//     console.log("You are not eligible");
+// }
+// let time=20;
+// if(time<=12){
+//     console.log("Good morning");
+
+// }   
+// else if(time>12 && time<=15){
+//     console.log("Good afternoon");
+// } 
+// else if(time>15 && time<=19){
+//     console.log("Good evening");
+// }
+// else{
+//     console.log("Good night");
+// }
+// let a=18;
+// (a<20)?console.log("a is les than 20"):console.log("a is than 20")
+// for(let i=0;i<11;i++){
+//     console.log(i);
+
+
+// let fruits=["Apple","Orange","Grapes","Banana"];
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+// =[1,2,3,4,5,6,];
+// let num=[1,2,3,4,5,6];
+// for(let i=0;i<num.length;i++){
+//     if(num[i]%2==0){
+//     console.log(`${num[i]}is even`)
+// } else{
+//     console.log(`${num[i]}is odd`)
+//  }
+// }
+// Find the sum of the array
+// let values=[1,2,3,4,5,6,7,8,9];
+// let sum=0;
+// for(let i=0;i<values.length;i++){
+//     sum=sum+values[i];
+// }
+// console.log(sum);
+// for(let i=1;i<=5;i++){
+//     for(j=1;j<=5;j++){
+//         console.log(`${i}*${j}=${i*j}`)
+//     }
+// }
+// REverse the string
+const str="Hello Nirmala!"
+for(let i=0)
+
